e4a5b036509759a826fc5024affd0789c9067cc6
Erich <89471455+SnellsCorner@users.noreply.github.com>
2022-09-08 17:05:07 -0400

    Add files via upload

---
+++ ISTE-340_09:08:2022_data.js
@@ -0,0 +1,7 @@
+
+const DATA = {
+    init:["choose", "men", "women"],
+    men: ["how do you like 'em", "tall", "short", ],
+    short: ["widht?", "husky like", "thin", ],
+    "husky like":["???", "x", "y"]
+}
