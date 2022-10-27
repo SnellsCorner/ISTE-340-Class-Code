254df97a200b57aafca15774f9e71c435996aeb4
Erich <89471455+SnellsCorner@users.noreply.github.com>
2022-10-27 17:03:59 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import getData from './util/getData.js';
+
+export default class App extends React.Component{
+
+  constructor(props){
+    //Below line creates access to what I normally assume is this
+    super(props);
+    //Any data that can and will change (so we get another render!)
+    this.state = {
+      about: {},
+      loaded: false
+    }
+  }
+
+  render(){
+    //bring in state...
+    const {about, loaded} = this.state;
+
+    if(!loaded) return (<div><h1>Loading...</h1></div>);
+
+    return (<div>
+          <h1>Welcome to the iSchool website...</h1>
+          <h2>{about.title}</h2>
+          <h4>{about.description}</h4>
+          <h3>{about.quote}</h3>
+          <h3>---{about.quoteAuthor}</h3>
+
+      </div>
+      );
+
+
+  }
+
+  componentDidMount(){
+    //Go get the data
+    getData('about/')
+      .then((json)=>{
+        console.log(json);
+        this.setState({
+          about: json, 
+          loaded: true
+        })
+      });
+  }
+
+  //on mounting - 1) constructor 2) getDerivedStateFromProps 3) render 4) componentDidMount
+
+}
