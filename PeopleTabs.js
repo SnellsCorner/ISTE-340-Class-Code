4d7072384b16006f60bbc9b72aea84a969d68ef5
Erich <89471455+SnellsCorner@users.noreply.github.com>
2022-11-04 10:20:02 -0400

    People Tabs

---
+++ PeopleTabs.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Tab } from 'semantic-ui-react';
+import AllPeople from './AllPeople.js';
+import getData from '../util/getData.js';
+
+
+export default class PeopleTabs extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            people: {},
+            loaded: false
+        }
+    }
+
+    render(){
+        const {people, loaded} = this.state;
+        const panes = [
+            { menuItem: 'Faculty', render: () => 
+                <Tab.Pane>
+                    <AllPeople key="1" title="Faculty" pepGroup={people.faculty}/>
+                </Tab.Pane> 
+            },
+            { menuItem: 'Staff', render: () => 
+                <Tab.Pane>
+                    <AllPeople key="2" title="Staff" pepGroup={people.staff}/>
+                </Tab.Pane> 
+            }
+        ]
+
+        if(!loaded)return(<div><h1>Our People are loading...</h1></div>);
+
+        return(
+            <div>
+                <h1>{people.title}</h1>
+                <h3>{people.subTitle}</h3>
+                <Tab panes={panes} />
+            </div>
+        );
+    }
+
+    componentDidMount(){
+        getData('people/')
+            .then((json) => {
+                this.setState({
+                    people:json,
+                    loaded: true
+                })
+            })
+    }
+}
